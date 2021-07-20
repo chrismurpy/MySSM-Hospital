2f7449740e65b3152d980a8a74fc15da3ac69c35
murphy <631341307@qq.com>
2021-07-20 15:04:56 +0800

    Initial commit - SSM环境初步搭建

---
+++ src/main/webapp/js/ckeditor/config.js
@@ -0,0 +1,15 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.width=700;
+	config.height=400;
+	config.toolbarStartupExpanded=false;
+	config.resize_enabled=false;
+};
